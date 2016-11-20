85f734264b781cd4efcdeaa5c4c8ea1376bf0c79
Yuliya Yachmenyuk <yachmenyuk@gmail.com>
2016-11-20 11:17:25 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+﻿var formRegistration = document.forms[0];
+var buttonRegistration = document.getElementById('registration_button');
+var registrationBlock = document.getElementById('registration');
+
+buttonRegistration.onclick = function() {
+			registrationBlock.style.opacity = 0;
+			
+		};
+
+
