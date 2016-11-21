70cc74f50e9d75565f3234726a9784e6a0bd124e
Yuliya Yachmenyuk <yachmenyuk@gmail.com>
2016-11-21 21:12:09 +0200

    add block list of ideas

--- js/script.js
+++ js/script.js
@@ -24,6 +24,25 @@
   
 };
 
+//change color like active 
+
+	$('.like').click(function() {
+		 $('.like').attr('src', 'img/icons_of_idea_list/like_active.png');
+	});
+
+	$('.unlike').click(function() {
+		 $('.unlike').attr('src', 'img/icons_of_idea_list/unlike_active.png');
+	});
+	$('.favorites_icon').click(function() {
+		 $('.favorites_icon').attr('src', 'img/icons_of_idea_list/active_favorites_star.png');
+	});
+
+
+
+
+
+
+
 
 $(document).ready(main);
 
